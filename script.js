2385bd8ff54b57213bd0890370967fe325497eb5
keirrisongreco <keirrison.greco@escola.pr.gov.br>
2026-09-03 08:25:37 -0300

    aula 5

--- script.js
+++ script.js
@@ -9,40 +9,65 @@ const textoResultado = document.querySelector(".texto-resultado");
   {
     enunciado:
       "Ao ter um trabalho em grupo, Artthur precisa decidir entre seus super amigos, fieis que nunca o trocariam ou uma garota falsa. O que ele escolheria?",
-    alternativas: ["A Garota!", "os Amigos maravilhosos!!!"],
+    alternativas: [
+      {
+        texto: "os Amigos maravilhosos!!!", 
+        afirmacao:"afirmacao"
+      },
+      {
+        texto: "A garota!",
+        afirmacao:  "afirmacao"
+      }
+     
+     
+    ]
   },
   {
     enunciado:
       "Ao escolher a garota falsa, artthur vai ao laboratório com ela e tem que escolher onde ira se sentar?",
     alternativas: [
-      "Longe de seus super amigos fiéis, pois ele gostaria de se tornar falso igual a ela",
-      "Perto dos seus super amigos fiéis que iriam o ajudar não importa as dificuldade, mesmo ele tendo feito escolhas ruins no passado.",
-    ],
+      {
+        texto: "Longe de seus super amigos fiéis, pois ele gostaria de se tornar falso igual a ela",
+        afirmacao:"afirmacao"
+      },
+     {
+      texto:  "Perto dos seus super amigos fiéis que iriam o ajudar não importa as dificuldade, mesmo ele tendo feito escolhas ruins no passado.",
+     }
+     
+    ]
   },
   {
     enunciado:
       "Depois de escolher onde se sentar, Artthur começa a fazer o trabalho até o sinal bater para o recreio e depois vai comer toda comida das escola. Ao voltar do recreio ele percebe que apagou os arquivos dele e as unicas pessoas que ficaram na sala depois que ele saiu  foram seus super amigos fiés e a garota falsa. Quem ele acusa?",
     alternativas: [
-      "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas",
-      "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+     {
+      texto:"Ele a acusa seus super amigos extremamente fieis",
+      afirmacao:"afirmacao"
+     },
+      {
+       texto:"Ele acusa a garota falsa.",
+       afirmacao:"afirmacao"
+      }
+      
     ],
   },
   {
     enunciado:
-      "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+      "Ao final da acusação Artthur descobre que foi a garota falsa, ele perdoa ela?",
     alternativas: [
-      "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-      "Criar uma imagem utilizando um gerador de imagem de IA.",
+      {
+        texto: "Ele perdoa ela.",
+        afirmacao:"afirmacao"
+      },
+      {
+        texto: "Ele decide não perdoar, voltar para seus super amigos e se arrepender de seus erros.",
+        afirmacao:"afirmacao"
+      }
+     
+     
     ],
   },
-  {
-    enunciado:
-      "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
-    alternativas: [
-      "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-      "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-    ],
-  },
+ 
 ];
 
 let atual = 0;
@@ -51,6 +76,19 @@ let perguntaAtual;
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
+   mostraAlternativas();
+}
+
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    botaoAlternativas.addEventListener("click", function(){
+      atual++:
+      mostraPergunta();
+    })
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
 }
 
 mostraPergunta();
