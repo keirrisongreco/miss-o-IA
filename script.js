73d4364b35a47f62e5e65cacbe470e8974431a80
keirrisongreco <keirrison.greco@escola.pr.gov.br>
2026-09-10 08:23:45 -0300

    aula 9

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function mostraAlternativas(){
    }
 }
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacoes;
+    const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
