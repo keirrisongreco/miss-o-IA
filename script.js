538db0f13ab44412c16a272889b1eef83c8a9bd4
keirrisongreco <keirrison.greco@escola.pr.gov.br>
2026-09-10 07:46:44 -0300

    afirmacao

--- script.js
+++ script.js
@@ -72,23 +72,39 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+  if(atual >= perguntas.length){
+    mostraResultado();
+    return;
+  }
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
-   mostraAlternativas();
+  caixaAlternativas.textContent = "";
+  mostraAlternativas();
+
 }
 
-function mostraAlternativas() {
-  for (const alternativa of perguntaAtual.alternativas) {
-    const botaoAlternativas = document.createElement("button");
-    botaoAlternativas.textContent = alternativa;
-    botaoAlternativas.addEventListener("click", function(){
-      atual++:
-      mostraPergunta();
-    })
-    caixaAlternativas.appendChild(botaoAlternativas);
-  }
+function mostraAlternativas(){
+   for(const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativas.textContent = alternativa.texto;
+      botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+      caixaAlternativas.appendChild(botaoAlternativas);
+   }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+      
 }
 
+function mostraResultado(){
+  caixaPerguntas.textContent = "futuramente...";
+  textoResultado.textContent = historiaFinal;
+  caixaAlternativas.textContent = "";
+}
 mostraPergunta();
