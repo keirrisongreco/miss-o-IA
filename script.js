544f696079943ff5a801b6706279db35a022e6b0
keirrisongreco <keirrison.greco@escola.pr.gov.br>
2026-09-24 10:37:29 -0300

    aula 1

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ const textoResultado = document.querySelector(".texto-resultado");
     alternativas: [
       {
         texto: "os Amigos maravilhosos!!!", 
-        afirmacao:"Ele usou a racionalidade e pensou na fidelidade dos seus amigos"
+        afirmacao:[
+          "Ele usou a racionalidade e pensou na fidelidade dos seus amigos", ""]
       },
       {
         texto: "A garota!",
-        afirmacao:  "Foi irracional e guiado por uma ilusao mental criada por ele mesmo"
+        afirmacao:  [
+          "Foi irracional e guiado por uma ilusao mental criada por ele mesmo", ""]
       }
      
      
@@ -28,11 +30,13 @@ const textoResultado = document.querySelector(".texto-resultado");
     alternativas: [
       {
         texto: "Longe de seus super amigos fiéis, pois ele gostaria de se tornar falso igual a ela",
-        afirmacao:"Ele nao se importou com eles"
+        afirmacao:[
+          "Ele nao se importou com eles", ""]
       },
      {
       texto:  "Perto dos seus super amigos fiéis que iriam o ajudar não importa as dificuldade, mesmo ele tendo feito escolhas ruins no passado.",
-        afirmacao:"Ele usou a racionalidade e bons sentimentos e decidiu nao se separar de seus amigos incriveis"
+        afirmacao:[
+          "Ele usou a racionalidade e bons sentimentos e decidiu nao se separar de seus amigos incriveis"]
     }
      
     ]
@@ -43,11 +47,13 @@ const textoResultado = document.querySelector(".texto-resultado");
     alternativas: [
      {
       texto:"Ele a acusa seus super amigos extremamente fieis",
-      afirmacao:"ele foi manipulado e iludido por ela"
+      afirmacao:[
+        "ele foi manipulado e iludido por ela", ""]
      },
       {
        texto:"Ele acusa a garota falsa.",
-       afirmacao:"ele caiu na realidade e lembra de todos os anos de amizade com seus amigos e que eles nunca fariam isso"
+       afirmacao:[
+        "ele caiu na realidade e lembra de todos os anos de amizade com seus amigos e que eles nunca fariam isso", ""]
       }
       
     ],
@@ -58,11 +64,12 @@ const textoResultado = document.querySelector(".texto-resultado");
     alternativas: [
       {
         texto: "Ele perdoa ela.",
-        afirmacao:"Ele se iludiu e foi manipualdo"
+        afirmacao:[
+          "Ele se iludiu e foi manipualdo", ""]
       },
       {
         texto: "Ele decide não perdoar, voltar para seus super amigos e se arrepender de seus erros.",
-        afirmacao:"Ele pensa nos seus amigo e quebra o ciclo de ilusao que ela implantou na mente dele"
+        afirmacao:["Ele pensa nos seus amigo e quebra o ciclo de ilusao que ela implantou na mente dele", ""]
       }
      
      
